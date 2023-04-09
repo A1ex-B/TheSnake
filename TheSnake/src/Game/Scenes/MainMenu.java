eb09b41a0a9a6db9a2bacdf169b7b660637ae2b8
A1ex <blinov.alex.s@gmail.com>
2023-04-09 15:10:50 +0300

    research about adding dependent libraries into jar

--- TheSnake/src/Game/Scenes/MainMenu.java
+++ TheSnake/src/Game/Scenes/MainMenu.java
@@ -1,8 +1,25 @@
 package Game.Scenes;
 
+import Game.RawConsoleInput;
+
+import java.io.IOException;
+
 public class MainMenu implements IScene {
     public SceneResult Run() {
         System.out.println("Hello!");
+        int ch = 0;
+        try {
+            ch = System.in.read();
+            System.in.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println("Ch = " + ch);
+        try {
+            var temp = RawConsoleInput.read(true);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
         return new SceneResult("Exit", true);
     }
 }
